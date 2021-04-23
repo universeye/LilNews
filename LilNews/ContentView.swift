80a3b35626f62ecc65d9ef89f0f8d263a3cd9cc8
Terry <terrykuo501@gmail.com>
2021-04-23 13:49:41 +0800

    Initial Commit

---
+++ LilNews/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LilNews
+//
+//  Created by Terry Kuo on 2021/4/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LilNews/LilNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LilNewsApp.swift
+//  LilNews
+//
+//  Created by Terry Kuo on 2021/4/23.
+//
+
+import SwiftUI
+
+@main
+struct LilNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
