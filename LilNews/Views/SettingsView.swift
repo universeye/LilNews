90bebde4492ead10105bab228372831a504e3bd6
Terry <terrykuo501@gmail.com>
2021-05-08 16:21:24 +0800

    Build Settings View

--- LilNews/Views/SettingsView.swift
+++ LilNews/Views/SettingsView.swift
@@ -9,7 +9,28 @@ import SwiftUI
 
 struct SettingsView: View {
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            Form {
+                
+                Section(header: Text("Display"), footer: Text("System settings will override Dark mode and use the current device theme")) {
+                    Toggle(isOn: .constant(true), label: {
+                        Text("Dark Mode")
+                    })
+                    Toggle(isOn: .constant(true), label: {
+                        Text("Use system settings")
+                    })
+                }
+                
+                Section {
+                    Label("Follow me on twitter @Argonyoyo", systemImage: "link")
+                }
+                .foregroundColor(.black)
+                .font(.system(size: 16, weight: .semibold))
+                
+                
+            }
+            .navigationTitle("Settings")
+        }
     }
 }
 
